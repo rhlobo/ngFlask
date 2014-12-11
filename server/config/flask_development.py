c0cb8f72851aeb8b4d64f224bb89a4a0bb8c03ca
rhlobo <rhlobo+github@gmail.com>
2014-12-11 18:43:03 -0200

    Created the expenses model.

--- server/config/flask_development.py
+++ server/config/flask_development.py
@@ -4,4 +4,4 @@ HOST = '0.0.0.0'
 SECRET_KEY = 'qwertyuiopasdfghjkl1234567890'
 
 # FLASK-SQLALCHEMY (DB)
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/app_tmp.db'
---
+++ server/domain/expense.py
@@ -0,0 +1,18 @@
+import datetime
+
+from server.app import db, api_manager
+
+
+def _get_date():
+    return datetime.datetime.now()
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_time = db.Column(db.DateTime, default=_get_date, nullable=False)
+    description = db.Column(db.Unicode, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    comment = db.Column(db.Unicode)
+
+
+blueprint = api_manager.create_api(Expense, methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
